0995e5e118bc41c291e341e3217463e0941718c0
keroles_youssef <kerolesyoussef028@gmail.com>
2023-04-12 15:04:43 +0200

    router home page problem solved

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,54 +1,39 @@
-import 'dart:async';
-import 'dart:io';
-
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:home_router/no_internet.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 import 'mobile_network.dart';
 
-ConnectivityResult? current;
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    if (Platform.isAndroid) WebView.platform = AndroidWebView();
-    if (Platform.isIOS) WebView.platform = CupertinoWebView();
-    StreamSubscription subscription = Connectivity()
-        .onConnectivityChanged
-        .listen((ConnectivityResult result) {
-      // Got a new connectivity status!
-      setState(() {
-        current = result;
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
-        child: current! == ConnectivityResult.wifi
-            ? Center(
-                child: const WebView(
-                  initialUrl: 'http://192.168.1.1/',
-                ),
-              )
-            : current! == ConnectivityResult.mobile
-                ? const MobileNetworkOffScreen()
-                : const NoInternetScreen(),
+        child: StreamBuilder<ConnectivityResult>(
+          stream: Connectivity().onConnectivityChanged,
+          builder: (context, snapShot) {
+            if (snapShot.connectionState == ConnectionState.active) {
+              if (snapShot.data == ConnectivityResult.wifi) {
+                return const WebviewScaffold(
+                  url: 'http://192.168.1.1',
+                  withJavascript: true,
+                  withLocalStorage: true,
+                  withZoom: true,
+                  ignoreSSLErrors: true, // Disable SSL certificate checks
+                );
+              } else if (snapShot.data == ConnectivityResult.mobile) {
+                return const MobileNetworkOffScreen();
+              } else {
+                return const NoInternetScreen();
+              }
+            } else {
+              return const NoInternetScreen();
+            }
+          },
+        ),
       ),
     );
   }
